a44965cc60a74b8922122a3cf56125b1db1bc3a4
Ricky Ultimate <rickyogigi@gmail.com>
2026-03-04 17:22:50 +0100

    feat: add JWT authentication middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import { verifyToken, TokenPayload } from "../utils/jwt.utils";
+import { sendError } from "../utils/response.utils";
+
+export interface AuthenticatedRequest extends Request {
+  user?: TokenPayload;
+}
+
+export const authenticate = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+): void => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    sendError(res, "Authorization token is required", 401);
+    return;
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = verifyToken(token);
+    req.user = decoded;
+    next();
+  } catch {
+    sendError(res, "Invalid or expired token", 401);
+  }
+};
