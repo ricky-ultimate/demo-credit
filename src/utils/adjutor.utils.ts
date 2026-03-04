1e8db9bc2007379b3ffef74c4841d25593fc648f
Ricky Ultimate <rickyogigi@gmail.com>
2026-03-04 17:19:45 +0100

    feat: add Adjutor Karma blacklist verification utility

---
+++ src/utils/adjutor.utils.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { ENV } from "../constants/env";
+import logger from "./logger.utils";
+
+export const isBlacklisted = async (email: string): Promise<boolean> => {
+  try {
+    const response = await axios.get(
+      `${ENV.ADJUTOR_BASE_URL}/v2/verification/karma/${email}`,
+      {
+        headers: {
+          Authorization: `Bearer ${ENV.ADJUTOR_API_KEY}`,
+        },
+      }
+    );
+
+    return response.data?.data !== null;
+  } catch (error: unknown) {
+    if (axios.isAxiosError(error) && error.response?.status === 404) {
+      return false;
+    }
+    logger("error", "Adjutor karma check failed:", error);
+    throw new Error("Unable to verify user identity. Please try again later.");
+  }
+};
