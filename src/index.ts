187aaceecd123b27c032690f62e2d5561c98f70a
Ricky Ultimate <rickyogigi@gmail.com>
2026-03-04 11:45:43 +0100

    feat: gate server startup on successful database connection

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 import { ENV } from "./constants/env";
 import logger from "./utils/logger.utils";
+import db from "./config/db.config";
 
 dotenv.config();
 
@@ -20,8 +21,16 @@ app.get("/health", (_req, res) => {
   res.status(200).json({ status: "ok" });
 });
 
-app.listen(ENV.PORT, () => {
-  logger("info", `Server running on port ${ENV.PORT} in ${ENV.NODE_ENV} mode`);
-});
+db.raw("SELECT 1")
+  .then(() => {
+    logger("info", "Database connection established");
+    app.listen(ENV.PORT, () => {
+      logger("info", `Server running on port ${ENV.PORT} in ${ENV.NODE_ENV} mode`);
+    });
+  })
+  .catch((err: unknown) => {
+    logger("error", "Database connection failed:", err);
+    process.exit(1);
+  });
 
 export default app;
